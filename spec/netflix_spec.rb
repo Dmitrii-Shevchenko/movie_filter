fc26ead22c833b6db260bc3ccc9f77f9c195063a
shidivr <shidivr@gmail.com>
2017-04-25 20:36:14 +0700

    renew with let

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -2,25 +2,30 @@ require 'spec_helper'
 require_relative '../netflix'
 
 describe Netflix do
-  before(:all) do
-    @net = Netflix.new('movies.txt' || ARGV[0])
-    @net.pay(5)
+  let(:netflix) {Netflix.new('movies.txt' || ARGV[0])}
+  let(:paid){netflix.pay(25)}
+  let(:minus_pay){netflix.pay(-30)}
+
+  it "should show movies when enough money" do
+    paid
+    expect(netflix.show(genre: 'Comedy', period: :new).class).to eq(Array)   
+  end
+
+  it "should raise error when not enough money" do 
+    expect {netflix.show(period: :new, genre: 'Comedy')}.to raise_error    
+  end  
+  
+  it "right preparing account " do 
+    paid
+    expect(netflix.acct).to eq(25)
   end
   
-  it "should show movies on request" do 
-    expect(@net.show(genre: 'Comedy', period: :new).class).to eq(Array)
-    expect {@net.show(period: :new, genre: 'Comedy')}.to raise_error    
-  end 
-  
-  it "should accepts money" do 
-    @net.pay(25)
-    expect(@net.acct).to eq(25)
-    @net.pay(-30)
-    expect(@net.acct).to be >= 0
+  it "wrong preparing account " do 
+    minus_pay
+    expect(netflix.acct).to be nil
   end
-  
+
   it "should shows cost of the movie" do 
-    @net.pay(5)
-    expect(@net.how_much?('Terminator')).to eq(3)
+    expect(netflix.how_much?('Terminator')).to eq(3)
   end
 end
