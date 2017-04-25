b85a15be6c877a11d3b774f8fa42ac3ebe105318
shidivr <shidivr@gmail.com>
2017-04-25 23:26:48 +0700

    renew specs

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -4,15 +4,21 @@ require_relative '../netflix'
 describe Netflix do
   let(:netflix) {Netflix.new('movies.txt' || ARGV[0])}
   let(:paid){netflix.pay(25)}
-
-  it "should show movies when enough money" do
-    paid
-    expect(netflix.show(genre: 'Comedy', period: :new).class).to eq(Array)   
+  
+  context "when enough money" do
+    before do
+      paid
+    end 
+    it "should show movies" do
+      expect(netflix.show(genre: 'Comedy', period: :new).class).to eq(Array)   
+    end
   end
 
-  it "should raise error when not enough money" do 
-    expect {netflix.show(period: :new, genre: 'Comedy')}.to raise_error    
-  end  
+  context "when not enough money" do
+    it "should raise error" do 
+      expect {netflix.show(period: :new, genre: 'Comedy')}.to raise_error    
+    end  
+  end
   
   describe '#pay' do 
     it 'changes account' do 
@@ -20,7 +26,7 @@ describe Netflix do
     end
      
     it 'fails when negative amount' do
-      expect {netflix.pay(-30)}.to change(netflix, :acct).by(0)
+      expect {netflix.pay(-30)}.not_to change(netflix, :acct)
     end 
   end
 
--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -2,16 +2,13 @@ require 'spec_helper'
 require_relative '../theatre'
 
 describe Theatre do
-  before(:all) do
-    @tht = Theatre.new('movies.txt' || ARGV[0])
-  end
+  let(:theatre) {Theatre.new('movies.txt' || ARGV[0])}
   
   it "shoud show movies on time" do
-    expect(@tht.show("12:20").class).to eq(Array)
+    expect(theatre.show("12:20").class).to eq(Array)
   end
   
   it "should show time" do
-    expect(@tht.when?('Laura')).to eq("since 12:00 before 18:00")
-    expect(@tht.when?('Psycho')).to eq("since 06:00 before 12:00 and since 12:00 before 18:00")
+    expect(theatre.when?('Psycho')).to eq("since 06:00 before 12:00 and since 12:00 before 18:00")
   end
 end
