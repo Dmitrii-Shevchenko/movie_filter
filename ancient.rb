e9874e140c34b199aa10f9096dd10b2e08ce1a2b
shidivr <shidivr@gmail.com>
2017-04-30 21:54:17 +0700

    renew

--- ancient.rb
+++ ancient.rb
@@ -1,6 +1,5 @@
 load 'movie.rb'
 class Ancient < Movie
-attr_reader :link, :title, :year, :country, :release, :genre, :time, :rate, :producer, :actors
   def initialize(movie)
     super(movie)
   end
--- new.rb
+++ new.rb
@@ -1,6 +1,5 @@
 load 'movie.rb'
 class New < Movie
-attr_reader :link, :title, :year, :country, :release, :genre, :time, :rate, :producer, :actors
   def initialize(movie)
     super(movie)
   end
