6d12b737a42ea09020946b7aa16661a2c00d84d1
shidivr <shidivr@gmail.com>
2017-05-01 20:21:46 +0700

    renew

--- spec/ancient_spec.rb
+++ spec/ancient_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 require_relative '../ancient'
 
 describe Ancient do
-  it '#inspect' do
-    expect(Ancient.new(["http://imdb.com/title/tt0031679/?ref_=chttp_tt_126","Mr. Smith Goes to Washington","1939","USA","1939-10-19","Drama","129 min","8.3","Frank Capra","James Stewart,Jean Arthur,Claude Rains"]).inspect).to eq('Mr. Smith Goes to Washington - старый фильм(1939 год)')
-  end
+#  subject{Ancient.new(["http://imdb.com/title/tt0031679/?ref_=chttp_tt_126","Mr. Smith Goes to Washington","1939","USA","1939-10-19","Drama","129 min","8.3","Frank Capra","James Stewart,Jean Arthur,Claude Rains"]) }
+#  it '#inspect' do
+#    expect(subject.inspect).to eq('Mr. Smith Goes to Washington - старый фильм(1939 год)')
+#  end
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,6 +1,29 @@
 require 'spec_helper'
 require_relative '../movie'
+require_relative '../ancient'
 
 describe Movie do
+  describe '#Movie.create' do
+    subject{mov = Movie.create(content,MovieCollection.new('movies.txt' || ARGV[0])).inspect }
 
+    context 'Ancient' do
+      let(:content) { ["","Ancient movie","1930","","","","","","",""] }
+      it { is_expected.to eq("Ancient movie - старый фильм(1930 год)") }
+    end
+    
+    context 'Classic' do
+      let(:content) { ["","Classic movie","1960","","","","","","producer",""] }
+      it { is_expected.to eq("Classic movie - классический фильм, режиссёр producer ()") }
+    end
+    
+    context 'Modern' do
+      let(:content) { ["","Modern movie","1970","","","","","","","1,2,3"] }
+      it { is_expected.to eq("Modern movie - cовременное кино: играют 1,2,3") }
+    end
+    
+    context 'New' do
+      let(:content) { ["","New movie","2010","","","","","","",""] }
+      it { is_expected.to eq("New movie - новинка, вышло 7 лет назад!") }
+    end
+  end
 end
