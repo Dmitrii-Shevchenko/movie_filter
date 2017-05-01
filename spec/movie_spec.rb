9cae35ba6f4c1b485566008a4dd011377dd770f6
shidivr <shidivr@gmail.com>
2017-05-01 21:15:33 +0700

    renew

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -4,26 +4,28 @@ require_relative '../ancient'
 
 describe Movie do
   describe '#Movie.create' do
-    subject{mov = Movie.create(content,MovieCollection.new('movies.txt' || ARGV[0])).inspect }
+    let(:mov_col){MovieCollection.new('movies.txt' || ARGV[0])}
+    let(:content){["http://imdb.com/title/tt0073195/?ref_=chttp_tt_209","Jaws",year,"USA","1975-06-20","Adventure,Drama,Thriller","124 min","8.1","Steven Spielberg","Roy Scheider,Robert Shaw,Richard Dreyfuss"]}
+    subject{Movie.create(content,mov_col).inspect }
 
     context 'Ancient' do
-      let(:content) { ["","Ancient movie","1930","","","","","","",""] }
-      it { is_expected.to eq("Ancient movie - старый фильм(1930 год)") }
+      let(:year) { '1930' }
+      it { is_expected.to eq("Jaws - старый фильм(1930 год)") }
     end
     
     context 'Classic' do
-      let(:content) { ["","Classic movie","1960","","","","","","producer",""] }
-      it { is_expected.to eq("Classic movie - классический фильм, режиссёр producer ()") }
+      let(:year) { '1960' }
+      it { is_expected.to eq("Jaws - классический фильм, режиссёр Steven Spielberg (Schindler's List,Saving Private Ryan,Raiders of the Lost Ark,Indiana Jones and the Last Crusade,Jurassic Park,Jaws)") }
     end
     
     context 'Modern' do
-      let(:content) { ["","Modern movie","1970","","","","","","","1,2,3"] }
-      it { is_expected.to eq("Modern movie - cовременное кино: играют 1,2,3") }
+      let(:year) { '1970' }
+      it { is_expected.to eq("Jaws - cовременное кино: играют Roy Scheider,Robert Shaw,Richard Dreyfuss") }
     end
     
     context 'New' do
-      let(:content) { ["","New movie","2010","","","","","","",""] }
-      it { is_expected.to eq("New movie - новинка, вышло 7 лет назад!") }
+      let(:year) { '2010' }
+      it { is_expected.to eq("Jaws - новинка, вышло 7 лет назад!") }
     end
   end
 end
